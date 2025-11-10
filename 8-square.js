3cb93e74e391f5c9caa39f15669dd21a32dff33c
Zubaida <zubaidasuleiman89@gmail.com>
2025-11-10 15:30:10 +0100

    "Add 8-square.js script that prints a square of X"

---
+++ 8-square.js
@@ -0,0 +1,12 @@
+const size = parseInt(process.argv[2]);
+
+if (isNaN(size)) {
+  console.log("Missing size");
+} else if (size > 0) {
+  let i = 0;
+  const line = "X".repeat(size);
+  while (i < size) {
+    console.log(line);
+    i++;
+  }
+}
